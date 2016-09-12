93ee6ada2f34c283838d9cd33c533348db28b287
platonarash <arash.rassoulpour@outlook.com>
2016-09-12 13:51:48 +0200

    added scilab

---
+++ artifacts/scilab/linux_scilab_install_v1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ -f /usr/bin/apt ] ; then
+    echo "Using APT package manager"
+    apt-get -y update && apt-get -y install scilab
+elif [ -f /usr/bin/yum ] ; then 
+    echo "Using YUM package manager"
+
+    yum -y update
+    yum install -y scilab
+
+fi
