48389aefc29143874238f700614eec0432ead58a
platonarash <arash.rassoulpour@outlook.com>
2016-09-12 08:40:07 +0200

    -y - yes2all

--- artifacts/haskell/linux_haskell_install_v1.1.sh
+++ artifacts/haskell/linux_haskell_install_v1.1.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -f /usr/bin/apt ] ; then
     echo "Using APT package manager"
-    apt-get -y update && apt-get install ghc ghc-prof ghc-doc
+    apt-get -y update && apt-get -y install ghc ghc-prof ghc-doc
 elif [ -f /usr/bin/yum ] ; then 
     echo "Using YUM package manager"
 
