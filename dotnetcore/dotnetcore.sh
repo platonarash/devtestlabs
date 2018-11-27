510f0bb904c5c6125248ea5456f5ce25b8087436
platonarash <arash.rassoulpour@outlook.com>
2018-11-27 13:23:12 +0100

    Update dotnetcore.sh

--- dotnetcore/dotnetcore.sh
+++ dotnetcore/dotnetcore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-key adv --keyserver packages.microsoft.com --recv-keys EB3E94ADBE1229CF
-apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys EB3E94ADBE1229CF
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 
-apt-get update
-apt-get install dotnet-sdk-2.1.105 
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1.105 
 
